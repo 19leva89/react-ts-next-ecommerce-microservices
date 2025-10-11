0d52ce10bf5e4d70f35b88ae95fd330630b5278b
Sobolev Dmitry <19leva89@gmail.com>
2025-10-11 18:49:41 +0300

    upd order-db build and start + upd product-db build and start

--- packages/order-db/src/index.ts
+++ packages/order-db/src/index.ts
@@ -1,2 +1,2 @@
-export { connectOrderDB } from './connection'
-export { Order, type OrderSchemaType, OrderStatus } from './order-model'
+export { connectOrderDB } from './connection.js'
+export { Order, type OrderSchemaType, OrderStatus } from './order-model.js'
--- packages/product-db/src/client.ts
+++ packages/product-db/src/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma'
+import { PrismaClient } from '../generated/prisma/index.js'
 
 const prismaClientSingleton = () => {
 	const isVercel = process.env.VERCEL === '1'
