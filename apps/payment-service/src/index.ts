74ae8ebc3307933de4999fc9323e57b1a168fba9
Dmitry Soboliev <19leva89@gmail.com>
2026-06-04 10:10:16 +0300

    upd dependencies + migrate to Devin

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { serve } from '@hono/node-server'
-import { clerkMiddleware } from '@hono/clerk-auth'
+import { clerkMiddleware } from '@clerk/hono'
 
 import sessionRoute from './routes/session.route.js'
 import webhookRoute from './routes/webhooks.route.js'
